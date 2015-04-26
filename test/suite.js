fe4b32807f2ece1008f6a93ce50a1e0ad2e4a8aa
Jarno Rantanen <jarno@jrw.fi>
2015-04-26 12:30:58 +0300

    Add test suite.

---
+++ test/suite.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var fs = require('fs');
+var child_process = require('child_process');
+
+var testCasePath = __dirname + '/cases/';
+var testCases = fs.readdirSync(testCasePath);
+
+describe('component-css-ns', function() {
+
+  testCases.forEach(function(dir) {
+
+    it(dir, function(done) {
+
+      var path = testCasePath + dir + '/';
+      var expected = fs.readFileSync(path + 'expected.out', 'utf8');
+
+      process.chdir(path);
+
+      child_process.exec('npm --silent test', function(err, stdOut, stdErr) {
+
+        if (err) assert.fail(dir + ': Could not run test command: ' + stdErr);
+
+        assert.equal(expected, stdOut);
+
+        done();
+
+      });
+    });
+  });
+});
