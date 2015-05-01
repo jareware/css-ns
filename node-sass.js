e6f69b6148d6013ec5df15b0867664f2f1f875bc
Jarno Rantanen <jarno@jrw.fi>
2015-05-01 15:21:15 +0300

    Fix behaviour with external libs on the --include-path.

--- node-sass.js
+++ node-sass.js
@@ -20,11 +20,14 @@ module.exports = function(url, prev, done) {
   var expectedExtension = path.extname(url) === '' ? path.extname(prev) : ''; // if the import had an extension (e.g. ".scss"), use that; if not, use the extension of the importing file
   var next = path.join(path.dirname(prev), url + expectedExtension); // construct import path relative to current file // TODO: Solve https://github.com/sass/node-sass/issues/762
   fs.readFile(next, function(err, data) {
-    if (err) return done(err); // could not read the source file, for whatever reason
-    try {
-      done({ contents: postcss([ transform({ filename: next }) ]).process(data).css });
-    } catch (e) {
-      done(e); // PostCSS transform failed
+    if (err) { // for whatever reason, couldn't read the source file (might exist on the --include-path for example)
+      done({ file: url }); // we wouldn't want to touch it anyway -> let SASS figure it out
+    } else {
+      try {
+        done({ contents: postcss([ transform({ filename: next }) ]).process(data).css });
+      } catch (e) {
+        done(e); // PostCSS transform failed
+      }
     }
   });
 };
