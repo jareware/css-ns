2601245dbbbe1a58bad350d4727a933f75bd15ab
Jarno Rantanen <jarno@jrw.fi>
2015-05-02 13:05:28 +0300

    Replace `transformTools.makeFalafelTransform()` with calling falafel directly.

--- browserify.js
+++ browserify.js
@@ -1,12 +1,15 @@
 var transformTools = require('browserify-transform-tools');
+var falafel = require('falafel');
 
 var CLASSNAME_SEARCH = /\b([a-z][a-zA-Z_-]*)/g;
 
-// @see https://github.com/benbria/browserify-transform-tools#creating-a-falafel-transform
-module.exports = transformTools.makeFalafelTransform('component-css-ns', {}, function(node, transformOptions, done) {
-  if (node.type === 'Property' && node.key.name === 'className') {
-    var componentNs = transformOptions.file.replace(/.*\/(\w+).*/, '$1');
-    node.value.update(node.value.source() + '.replace(' + CLASSNAME_SEARCH.toString() + ', "' + componentNs + '-$1")');
-  }
-  done();
+// @see https://github.com/benbria/browserify-transform-tools#creating-a-string-transform
+module.exports = transformTools.makeStringTransform('component-css-ns', {}, function(content, transformOptions, done) {
+  content = falafel(content, function(node) {
+    if (node.type === 'Property' && node.key.name === 'className') {
+      var componentNs = transformOptions.file.replace(/.*\/(\w+).*/, '$1');
+      node.value.update(node.value.source() + '.replace(' + CLASSNAME_SEARCH.toString() + ', "' + componentNs + '-$1")');
+    }
+  });
+  done(null, content);
 });
