9496e7500c18d9e885f58403d893854dd1568b0e
Jarno Rantanen <jarno@jrw.fi>
2015-08-05 19:42:06 +0300

    Back to basics.

--- test/cases/core/test.js
+++
@@ -1,7 +0,0 @@
-var core = require('component-css-ns/core');
-
-// Make sure we're testing the path ascension properly
-console.log('pwd:', process.cwd().substr(__dirname.length));
-
-// Test loading config file
-console.log('configFile:', core.loadConfigFile('whatever.css'));
