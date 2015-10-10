3c2dba826eca118dec5b4a943e05d3c9ff523b70
Jarno Rantanen <jarno@jrw.fi>
2015-10-10 14:21:26 +0300

    Remove repetition from test cases.

--- css-ns.spec.js
+++ css-ns.spec.js
@@ -7,40 +7,42 @@ describe('css-ns', function() {
 
   describe('React support', function() {
 
+    function assertEqualHtml(Component, expectedHtml) {
+      assert.deepEqual(
+        ReactDOMServer.renderToStaticMarkup(React.createElement(Component)),
+        expectedHtml
+      );
+    }
+
     it('prefixes a single className', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement('div', { className: 'row' })
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="MyComponent-row"></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="MyComponent-row"></div>'
+      );
     });
 
     it('prefixes classNames recursively', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement('div', { className: 'row' },
             React.createElement('div', { className: 'column' })
           )
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="MyComponent-row"><div class="MyComponent-column"></div></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="MyComponent-row"><div class="MyComponent-column"></div></div>'
+      );
     });
 
     it('allows elements without a className', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement('div', { className: 'row' },
             React.createElement('section', null,
               React.createElement('div', { className: 'column' })
@@ -48,64 +50,63 @@ describe('css-ns', function() {
           )
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="MyComponent-row"><section><div class="MyComponent-column"></div></section></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="MyComponent-row"><section><div class="MyComponent-column"></div></section></div>'
+      );
     });
 
     it('respects component boundaries', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyChildComponent = function() {
         return React.createElement('div', { className: 'protected' });
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement('div', { className: 'container' },
             React.createElement(MyChildComponent, null)
           )
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="MyComponent-container"><div class="protected"></div></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="MyComponent-container"><div class="protected"></div></div>'
+      );
     });
 
     it('respects ownership of children across components', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyChildComponent = function(props) {
         return React.createElement('div', { className: 'protected' }, props.children);
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement(MyChildComponent, null,
             React.createElement('div', { className: 'owned' })
           )
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="protected"><div class="MyComponent-owned"></div></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="protected"><div class="MyComponent-owned"></div></div>'
+      );
     });
 
     it('works with nested components', function() {
-      var options = {
-        namespace: 'MyComponent'
-      };
       var MyChildComponent = function() {
         return cssNs.nsReactTree({ namespace: 'MyChildComponent' },
           React.createElement('div', { className: 'protected' })
         );
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree(options,
+        return cssNs.nsReactTree({ namespace: 'MyComponent' },
           React.createElement('div', { className: 'container' },
             React.createElement(MyChildComponent, null)
           )
         );
       };
-      var html = ReactDOMServer.renderToStaticMarkup(React.createElement(MyComponent));
-      assert.deepEqual(html, '<div class="MyComponent-container"><div class="MyChildComponent-protected"></div></div>');
+      assertEqualHtml(
+        MyComponent,
+        '<div class="MyComponent-container"><div class="MyChildComponent-protected"></div></div>'
+      );
     });
 
   });
