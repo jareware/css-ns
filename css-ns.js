72fde1d649aa5b47e0c2a436916f770c472d9f1c
Jarno Rantanen <jarno@jrw.fi>
2015-10-10 14:51:21 +0300

    Use new makeOptions() to simplify nsReactTree() slightly.

--- css-ns.js
+++ css-ns.js
@@ -24,8 +24,9 @@ function makeOptions(raw) {
 }
 
 function nsReactTree(options, el) {
+  var opts = makeOptions(options);
   if (!React.isValidElement(el)) return el; // this is something we don't understand -> leave it alone
-  var props = el.props.className ? { className: options.namespace + '-' + el.props.className } : el.props;
-  var children = React.Children.map(el.props.children, nsReactTree.bind(null, options));
+  var props = el.props.className ? { className: opts.namespace + '-' + el.props.className } : el.props;
+  var children = React.Children.map(el.props.children, nsReactTree.bind(null, opts));
   return React.cloneElement(el, props, children);
 }
--- css-ns.spec.js
+++ css-ns.spec.js
@@ -41,7 +41,7 @@ describe('css-ns', function() {
 
     it('prefixes a single className', function() {
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement('div', { className: 'row' })
         );
       };
@@ -53,7 +53,7 @@ describe('css-ns', function() {
 
     it('prefixes classNames recursively', function() {
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement('div', { className: 'row' },
             React.createElement('div', { className: 'column' })
           )
@@ -67,7 +67,7 @@ describe('css-ns', function() {
 
     it('allows elements without a className', function() {
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement('div', { className: 'row' },
             React.createElement('section', null,
               React.createElement('div', { className: 'column' })
@@ -86,7 +86,7 @@ describe('css-ns', function() {
         return React.createElement('div', { className: 'protected' });
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement('div', { className: 'container' },
             React.createElement(MyChildComponent, null)
           )
@@ -103,7 +103,7 @@ describe('css-ns', function() {
         return React.createElement('div', { className: 'protected' }, props.children);
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement(MyChildComponent, null,
             React.createElement('div', { className: 'owned' })
           )
@@ -117,12 +117,12 @@ describe('css-ns', function() {
 
     it('works with nested components', function() {
       var MyChildComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyChildComponent' },
+        return cssNs.nsReactTree('MyChildComponent',
           React.createElement('div', { className: 'protected' })
         );
       };
       var MyComponent = function() {
-        return cssNs.nsReactTree({ namespace: 'MyComponent' },
+        return cssNs.nsReactTree('MyComponent',
           React.createElement('div', { className: 'container' },
             React.createElement(MyChildComponent, null)
           )
