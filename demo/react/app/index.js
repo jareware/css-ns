24a65a4ce685d626f8f087825ea1f2f9b243b37d
Jarno Rantanen <jarno@jrw.fi>
2015-10-11 22:10:30 +0300

    Add first version of the ReactDemoApp component.

--- demo/react/app/index.js
+++ demo/react/app/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDemoApp from './ui/ReactDemoApp';
 
-console.info('Hello World!');
+ReactDOM.render(<ReactDemoApp />, document.querySelector('#container'));
---
+++ demo/react/app/ui/ReactDemoApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ReactDemoApp extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+
+}
