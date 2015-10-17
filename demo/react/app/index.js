057c7b1fa9302cdf02cf347e0fd20a7a51c010a5
Jarno Rantanen <jarno@jrw.fi>
2015-10-17 18:14:13 +0300

    Add a simple Date.now() based benchmark as well.

--- demo/react/app/index.js
+++ demo/react/app/index.js
@@ -4,9 +4,13 @@ import Perf from 'react-addons-perf';
 import ReactDemoApp from './ui/ReactDemoApp';
 
 Perf.start(); // see https://facebook.github.io/react/docs/perf.html
-
+const then = Date.now();
 ReactDOM.render(<ReactDemoApp />, document.querySelector('#container'));
-
+const now = Date.now();
 Perf.stop();
+
+// for NODE_ENV=production
+console.log('Total render time:', now - then, 'ms');
+
 Perf.printInclusive();
 Perf.printExclusive();
