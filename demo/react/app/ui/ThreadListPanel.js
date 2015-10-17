e4862e61e30142acfee15960d2a80cacca9e0b2b
Jarno Rantanen <jarno@jrw.fi>
2015-10-17 13:00:47 +0300

    Add initial implementation of ThreadListPanel.

--- demo/react/app/ui/ThreadListPanel.js
+++ demo/react/app/ui/ThreadListPanel.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import { getRandomNames, getRandomMessages, getRandomColors } from '../util/data';
 
 const ns = require('../util/css-ns')(__filename);
 
 export default class extends React.Component {
 
   render() {
+    const names = getRandomNames();
+    const messages = getRandomMessages();
+    const colors = getRandomColors();
     return ns(
       <Panel className="this" header="Your conversations" bsStyle="primary">
-        TODO
+        {names.map((name, index) => (
+          <div key={index} className="thread">
+            <div className="badge" style={{ background: colors[index] }}>{name.substr(0, 1)}</div>
+            <div className="details">
+              <div className="name">{name}</div>
+              <div className="message">{messages[index]}</div>
+            </div>
+          </div>
+        ))}
       </Panel>
     );
   }
