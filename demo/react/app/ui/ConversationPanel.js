c48c5d498892f833e3dfb473b57ec67f0672df27
Jarno Rantanen <jarno@jrw.fi>
2015-10-17 13:58:59 +0300

    Add initial implementation of ConversationPanel.

--- demo/react/app/ui/ConversationPanel.js
+++ demo/react/app/ui/ConversationPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import { getRandomMessages } from '../util/data';
 
 const ns = require('../util/css-ns')(__filename);
 
@@ -8,7 +9,11 @@ export default class extends React.Component {
   render() {
     return ns(
       <Panel className="this">
-        TODO
+        {getRandomMessages().map((message, index) => (
+          <div key={index} className={{ message: true, mine: Math.random() > 0.5 }}>
+            {message}
+          </div>
+        ))}
       </Panel>
     );
   }
