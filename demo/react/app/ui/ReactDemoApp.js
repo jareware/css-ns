cf22bfe5e86bddcf37032e6ec2bc7813ba24b1f4
Jarno Rantanen <jarno@jrw.fi>
2015-10-17 12:16:09 +0300

    Rename MessagesPanel.

--- demo/react/app/ui/ReactDemoApp.js
+++ demo/react/app/ui/ReactDemoApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import createCssNs from 'css-ns';
 import { Panel } from 'react-bootstrap';
-import MessagesPanel from './MessagesPanel';
+import ThreadListPanel from './ThreadListPanel';
 import ConversationPanel from './ConversationPanel';
 
 const ns = require('../util/css-ns')(__filename);
@@ -11,7 +11,7 @@ export default class extends React.Component {
   render() {
     return ns(
       <div className="this">
-        <MessagesPanel />
+        <ThreadListPanel />
         <ConversationPanel />
       </div>
     );
